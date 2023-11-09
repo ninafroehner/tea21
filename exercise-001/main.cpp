867913f8fd2f4264ceb266d6222bb3838145db0e
ninafroehner <150281591+ninafroehner@users.noreply.github.com>
2023-11-09 22:48:26 +0100

    changed main.cpp

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -7,29 +7,47 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+// .bss segment
+int bss;
+
+// .data segment
+int data = 4711;
+
+// .rodata segement
+const int rodata =42;
+
+
 auto main(int argc, char **argv) -> int
+
 {
-    auto count{20};
-    /**
-     * CLI11 is a command line parser to add command line options
-     * More info at https://github.com/CLIUtils/CLI11#usage
-     */
-    CLI::App app{PROJECT_NAME};
-    try
-    {
-        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        app.parse(argc, argv);
-    }
-    catch (const CLI::ParseError &e)
-    {
-        return app.exit(e);
-    }
+    int stack;
+    int stack2 = 42;
 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+
+    fmt::print("Value of variable bss {} address of variable bss {}\n",
+    bss,
+    fmt::ptr(&bss));
+
+    fmt::print("Value of variable data {} address of variable data {}\n",
+    data,
+    fmt::ptr(&data));
+
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n",
+    rodata,
+    fmt::ptr(&rodata));
+
+    fmt::print("Value of variable stack {} address of variable stack {}\n",
+    stack,
+    fmt::ptr(&stack));
+
+    fmt::print("Value of variable stack2 {} address of variable stack2 {}\n",
+    stack2,
+    fmt::ptr(&stack2));
+
     return 0; /* exit gracefully*/
 }
