86bca59ac3ac9582ef22d11a456d542232a2e256
ninafroehner <150281591+ninafroehner@users.noreply.github.com>
2023-11-10 11:35:59 +0100

    test

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -26,6 +26,9 @@ auto main(int argc, char **argv) -> int
         return app.exit(e);
     }
 
+    std::vector<int> data(count);
+    fmt::print("Created a vector with {} elements \n", count);
+
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
